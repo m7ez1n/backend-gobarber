a1ae09e6e351cc26ed0387b73da738735a9373d0
m7Aei <mateuspm2015@hotmail.com>
2019-09-11 18:47:00 -0300

    Configurando os dados do mailtrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8bf2f8989e514d',
+    pass: 'a13a242e54cf02',
+  },
+  default: {
+    from: 'Equipe de desenvolvedores gobarber <noreply@gobarber.com>',
+  },
+};
